5b87e4a87ebd4fdeab10df812706bf57c998a034
Alex Hoff <fishfin365@icloud.com>
2026-03-16 14:54:03 -0500

    made starting screen and login screen
    I made the frontend for the start and login screen. not completely finished, but once the backend stuff i can hook it up tot eh buttons/text inputs and whatnot. Ill start working on making the frontend for the main profile page tonight or tomorrow.

---
+++ app/src/main/java/loginfunctionality.java
@@ -0,0 +1,35 @@
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+//import com.example.drinkinventoryapp.databinding.ActivityMainBinding;
+
+public class loginfunctionality {
+    final Button login_button;
+    final Button forgot_password_button;
+
+    public loginfunctionality(Button loginButton, Button forgotPasswordButton) {
+        login_button = loginButton;
+        forgot_password_button = forgotPasswordButton;
+    }
+
+    public void onCreate(){
+        //binding = ActivityMainBinding.inflate(layoutInflater);
+
+
+        //binding.login_button.setOnClickListener{
+
+        //}
+
+        //binding.forgot_password_button{
+
+        //}
+
+
+
+
+    }
+}
