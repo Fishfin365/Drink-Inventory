1ee111dada2a8fe6b9002eb62033a257897b6eb1
isaacwinkelman <ijwinkelman0223@gmail.com>
2026-03-18 14:51:01 -0500

    Added FirebaseDB
    Implemented firebase app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    //id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -54,6 +56,8 @@ dependencies {
     //firebase
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
+    implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
